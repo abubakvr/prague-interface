dc3c87bcba836b3ae175e5e09aff66ce8969fcbf
sadeeqdev <boskitechh@gmail.com>
2025-03-08 23:53:47 +0100

    fix: fix email sent page

--- src/app/(auth)/email-sent/page.tsx
+++ src/app/(auth)/email-sent/page.tsx
@@ -85,7 +85,7 @@ const EmailContentLoading = () => (
 // Main page component with Suspense boundary
 const EmailSentPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex pt-6 md:pt-16 justify-center bg-gray-100">
       <Suspense fallback={<EmailContentLoading />}>
         <EmailContent />
       </Suspense>
